cf674ee720d6bab62391442a40f7737fd1f8e121
Gergan Nikolov <gergan.n@logicpromo.com>
2017-06-12 17:39:30 +0300

    Product popup added

--- js/initial.js
+++ js/initial.js
@@ -24,6 +24,46 @@ jQuery( document ).ready(function(){
 	        }
 	    }
 	});
+
+    if ( jQuery( "body" ).hasClass( "page-template-store" ) ) {
+        jQuery( ".page-template-store" ).find( ".buy-button" ).each( function(){
+            jQuery( this ).on( "click", function(){
+                view_ = "\
+                <div id='popup'>\
+                    <div id='inner-popup'>\
+                        <button id='close-button'>\
+                            <span class='bar'></span>\
+                            <span class='bar'></span>\
+                        </button>\
+                        <div class='product-info'>\
+                            <h1 class='product-title'>Product Name</h1>\
+                            <h2 class='product-price'>15lv</h2>\
+                        </div>\
+                        <div class='paypal-button-handler'></div>\
+                        <div class='product-content'>\
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\
+                        </div>\
+                        <div class='product-reviews'>\
+                            <a href='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' target='_blank' class='image-anchor'>\
+                                <img src='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' class='image' />\
+                            </a>\
+                            <a href='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' target='_blank' class='image-anchor'>\
+                                <img src='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' class='image' />\
+                            </a>\
+                            <a href='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' target='_blank' class='image-anchor'>\
+                                <img src='https://images.unsplash.com/photo-1465308452258-70fc6a9b67a8' class='image' />\
+                            </a>\
+                        </div>\
+                    </div>\
+                </div>\
+                ";
+
+                jQuery( "body" ).append( view_ );
+
+                jQuery( "#popup #close-button" ).on( "click", function(){ jQuery( "#popup" ).remove(); } );
+            } );
+        } );
+    }
 });
 
 function slideTo( element ) {
